8dce19a6a6711b5496cfd8f0f491a07ec344f941
mildtamon <mildtamon@gmail.com>
2023-06-17 22:43:20 +0700

    add helpCommand

--- src/main/java/io/muic/ssc/zork/command/CommandType.java
+++ src/main/java/io/muic/ssc/zork/command/CommandType.java
@@ -1,11 +1,13 @@
 package io.muic.ssc.zork.command;
 
 import io.muic.ssc.zork.command.impl.ExitCommand;
+import io.muic.ssc.zork.command.impl.HelpCommand;
 import io.muic.ssc.zork.command.impl.InfoCommand;
 
 public enum CommandType {
     INFO(InfoCommand.class, "info"),
-    EXIT(ExitCommand.class, "exit");
+    EXIT(ExitCommand.class, "exit"),
+    HELP(HelpCommand.class, "help");
 
     private Class<? extends Command> commandClass;      // need to get class -> so we know which command class to execute
     private String commandWord;
---
+++ src/main/java/io/muic/ssc/zork/command/impl/HelpCommand.java
@@ -0,0 +1,26 @@
+package io.muic.ssc.zork.command.impl;
+
+import io.muic.ssc.zork.Game;
+import io.muic.ssc.zork.command.Command;
+
+import java.io.*;
+
+public class HelpCommand implements Command {
+
+    @Override
+    public void execute(Game game, String argument) {
+        if (argument != null) {
+            System.out.println("exit does not have any argument, please try again");
+        } else {
+            try (FileReader fileReader = new FileReader("src/main/java/io/muic/ssc/zork/allCommandInfo.txt");) {
+                int content;
+                while ((content = fileReader.read()) != -1) {
+                    System.out.print((char) content);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+//                System.out.println("file not found");
+            }
+        }
+    }
+}
